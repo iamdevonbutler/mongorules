74dccee3bbc79840a57d0a75a7d4bb055063d4ac
iamdevonbutler <jpescione@gmail.com>
2017-02-12 11:06:33 -0500

    all tests passing for addtoset/push

--- tests/integration/update.arrayofobjects.test.js
+++ tests/integration/update.arrayofobjects.test.js
@@ -82,123 +82,100 @@ describe('Update(): array of objects:', function() {
       ]);
     });
   });
+
+  describe('$addToSet', function() {
+    it('should add a single item to the nicknames array using the item in array syntax', function* () {
+      var payload = {
+        '$addToSet': {
+          'account.friends.0.nicknames': {
+            name: 'lou',
+            giver: []
+          }
+        }
+      };
+      yield db.users3.update({}, payload);
+      var result = yield db.users3.findOne({});
+      result.account.friends.should.eql([
+        {
+          name: 'lrn!',
+          nicknames: [{name: 'lou', giver: []}]
+        }, {
+          name: 'gus!',
+          nicknames: []
+        }
+      ]);
+    });
   //
-  // describe('$addToSet', function() {
-  //   it('should add a single item to the nicknames array using the item in array syntax', function* () {
-  //     var payload = {
-  //       '$addToSet': {
-  //         'account.friends.0.nicknames': {
-  //           name: 'lou',
-  //           giver: []
-  //         }
-  //       }
-  //     };
-  //     yield db.users3.update({}, payload);
-  //     var result = yield db.users3.findOne({});
-  //     result.account.friends.should.eql([
-  //       {
-  //         name: 'lrn!',
-  //         nicknames: [
-  //           {
-  //             name: 'lou',
-  //             giver: []
-  //           }
-  //         ]
-  //       }, {
-  //         name: 'gus!',
-  //         nicknames: []
-  //       }
-  //     ]);
-  //   });
-  //
-  //   it('should should throw an error given an incorrect type for an object property', function* () {
-  //     var payload = {
-  //       '$addToSet': {
-  //         'account.friends': {
-  //           '$each': [
-  //             {
-  //               name: 'jay',
-  //               nicknames: 1
-  //             }
-  //           ]
-  //         }
-  //       }
-  //     };
-  //     try {
-  //       yield db.users3.update({}, payload);
-  //       exit();
-  //     }
-  //     catch (e) {
-  //       e.errors[0].property.should.eql('type');
-  //     }
-  //   });
-  //
-  //   it('should should throw an error given a missing object field', function* () {
-  //     var payload = {
-  //       '$addToSet': {
-  //         'account.friends': {
-  //           '$each': [
-  //             {
-  //               name: 'jay',
-  //               nicknames: [
-  //                 {
-  //                   name: 'bird'
-  //                 }
-  //               ]
-  //             }
-  //           ]
-  //         }
-  //       }
-  //     };
-  //     try {
-  //       yield db.users3.update({}, payload);
-  //       exit();
-  //     }
-  //     catch (e) {
-  //       e.errors[0].property.should.eql('required');
-  //     }
-  //   });
-  //
-  //   it('should add an item to set using $each', function* () {
-  //     var _document, output, payload;
-  //     _document = {
-  //       name: 'jay',
-  //       nicknames: [
-  //         {
-  //           name: 'bird',
-  //           giver: [
-  //             {
-  //               name: 'gus'
-  //             }
-  //           ]
-  //         }
-  //       ]
-  //     };
-  //     output = {
-  //       name: 'jay!',
-  //       nicknames: [
-  //         {
-  //           name: 'bird',
-  //           giver: [
-  //             {
-  //               name: 'gus'
-  //             }
-  //           ]
-  //         }
-  //       ]
-  //     };
-  //     payload = {
-  //       '$addToSet': {
-  //         'account.friends': {
-  //           '$each': [_document]
-  //         }
-  //       }
-  //     };
-  //     yield db.users3.update({}, payload);
-  //     var result = yield db.users3.findOne({});
-  //     result.account.friends.length.should.eql(3);
-  //     result.account.friends[2].should.eql(output);
-  //   });
-  // });
+    it('should should throw an error given an incorrect type for an object property', function* () {
+      var payload = {
+        '$addToSet': {
+          'account.friends': {
+            '$each': [{
+              name: 'jay',
+              nicknames: 1
+            }]
+          }
+        }
+      };
+      try {
+        yield db.users3.update({}, payload);
+        exit();
+      }
+      catch (e) {
+        e.errors[0].property.should.eql('type');
+      }
+    });
+
+    it('should should throw an error given a missing object field', function* () {
+      var payload = {
+        '$addToSet': {
+          'account.friends': {
+            '$each': [{
+               name: 'jay',
+               nicknames: [{name: 'bird'}]
+             }]
+          }
+        }
+      };
+      try {
+        yield db.users3.update({}, payload);
+        exit();
+      }
+      catch (e) {
+        e.errors[0].property.should.eql('required');
+      }
+    });
+
+    it('should add an item to set using $each', function* () {
+      var _document = {
+        name: 'jay',
+        nicknames: [{
+          name: 'bird',
+          giver: [{name: 'gus'}]
+        }]
+      };
+      var output = {
+        name: 'jay!',
+        nicknames: [{
+            name: 'bird',
+            giver: [{
+              name: 'gus'
+            }]
+          }
+        ]
+      };
+      var payload = {
+        '$addToSet': {
+          'account.friends': {
+            '$each': [_document]
+          }
+        }
+      };
+      yield db.users3.update({}, payload);
+      var result = yield db.users3.findOne({});
+      result.account.friends.length.should.eql(3);
+      result.account.friends[2].should.eql(output);
+    });
+  });
 
 });
